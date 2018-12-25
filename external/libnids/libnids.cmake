5dc3db4848956891f329284f0ee88cd11166fedd
zhouweikang <zhouweikang@dianjiutong.cn>
2018-12-25 20:03:01 +0800

    add libnids

--- external/libnids/libnids.cmake
+++ external/libnids/libnids.cmake
@@ -7,15 +7,15 @@ ExternalProject_Add(libnids
 	URL               "https://managedway.dl.sourceforge.net/project/libnids/libnids/1.24/libnids-1.24.tar.gz"
 	URL_MD5           72d37c79c85615ffe158aa524d649610
     #DOWNLOAD_COMMAND ${EXTERNAL_CURRENT_DOWNLOAD_COMMAND}
+	BUILD_IN_SOURCE 1
 	#CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/external/libnids/src/libnids/configure --prefix=${CMAKE_BINARY_DIR}/external/libnids
 	CONFIGURE_COMMAND ./configure --prefix=${CMAKE_BINARY_DIR}/external/libnids
     BUILD_COMMAND     make
     INSTALL_COMMAND   make install
-	BUILD_IN_SOURCE 1
     )
 
 add_library(nids STATIC IMPORTED)
 set(NIDS_FOUND "TRUE")
-set(NIDS_INCLUDE_DIRS "${EXTERNAL_CURRENT_BIN_DIR}/include")
-set(NIDS_LIBRARIES "xz")
+set(NIDS_INCLUDE_DIRS "${CMAKE_BINARY_DIR}/external/libnids/include")
+set(NIDS_LIBRARIES "${CMAKE_BINARY_DIR}/external/libnids/lib/libnids.a")
 
